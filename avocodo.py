686260962014d44b5df59ffa7f5d5505b5a54e34
tjards <travis.jardine@gmail.com>
2023-02-13 19:51:52 -0500

    added origin

--- avocodo.py
+++ avocodo.py
@@ -6,27 +6,25 @@ Created on Sun Feb 12 13:36:32 2023
 @author: tjards
 """
 
-#avocodo
+# import stuff
+# ------------
 import random
 import time
 
+# main function
+# ------------
 def main():
-
     print('Me: Are you ready yet?')
-    
-    i = random.randint(1,10)
-    
+    i = random.randint(1,30)
     while i > 0:
-        time.sleep(1)
         print('Avocado: No, not yet.')
+        time.sleep(0.5)
         i -=1
-        if i == 1:
-            print('Avocado:OK, now.')
-            break
         print('Me: How about now?')
+    print('Avocado: OK, now.')
     print('Avocado: Too late.')
 
-
-if __name__ == "__main__":
-    
+# terminal call
+# -------------
+if __name__ == "__main__":  
     main()
